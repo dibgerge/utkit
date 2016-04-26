25eceec45c2db866c2089cc09d39dda35a3385e9
Gerges <dibgerge@gmail.com>
2016-04-25 17:06:47 -0700

    Added IO to read from CIVA and ultravision.

---
+++ io/civa.py
@@ -0,0 +1,34 @@
+import numpy as np
+import utkit
+import re
+
+
+def civa_bscan(fname):
+    """
+    Reads a bscan file saved in CIVA.
+    Parameters:
+        fname:
+            Name of the file, including the fullpath if not in the current directory
+        Returns:
+            Bscan (:class:`utkit.uFrame`):
+              A uFrame object containing the B-scan
+    """
+    # this is the default start of the header in a civa b-scan txt file
+    skip_lines = 12
+    # read the header
+    with open(fname) as fid:
+        for i, line in enumerate(fid):
+            if i == skip_lines-1:
+                coords = re.findall(r'\d*\.?\d+', line)
+                coords = np.array([float(val) for val in coords])
+                cols = line.split(';')
+                ind = np.array([j for j, c in enumerate(cols) if 'val' in c])
+                break
+
+    d = np.genfromtxt(fname,  delimiter=';', skip_header=skip_lines)
+    # convert from microseconds in CIVA b-scan file to seconds
+    Y = d[:, 0]*1e-6
+    # convert from millimeters to meters
+    X = coords[ind-1]*1e-3
+    b = d[:, ind]
+    return utkit.uFrame(b, index=Y, columns=X)
---
+++ io/ultravision.py
@@ -0,0 +1,43 @@
+"""
+Reads ultrasound scans saved in UltraVision (ZETEC software) text file format and returns a
+rasterScan object.
+"""
+import pandas as pd
+import numpy as np
+import utkit
+
+
+def readuv(fname):
+    """
+    :param
+        fname: file name
+        Fs: Sampling frequency (Hz)
+
+
+    :return:
+    """
+    header = pd.read_table(fname, sep=' =\t', skipinitialspace=True, header=None, nrows=19,
+                           engine='python', index_col=0, squeeze=True)
+
+    t = np.arange(int(header['USoundQty (sample)']))*float(header['USoundResol [True Depth] (mm)'])*1e-3
+
+    # convert x/y indices to meters (from millimeters) in the uv file
+    Xv = np.arange(int(header['ScanQty (sample)'])) * float(header['ScanResol (mm)'])*1e-3
+    Yv = np.arange(int(header['IndexQty (sample)'])) * float(header['IndexResol (mm)'])*1e-3
+    X, Y = np.meshgrid(Xv, Yv)
+    u = pd.read_table(fname,
+                      skiprows=19,
+                      sep='\t',
+                      skipinitialspace=True,
+                      header=None,
+                      engine='c',
+                      index_col=False)
+    # the last column is giving NaN values. Just remove it.
+    u = u.iloc[:, :-1]
+    u.index = [X.ravel(), Y.ravel()]
+    u.columns = t
+    p = u.to_panel()
+    return utkit.RasterScan(p.transpose(2, 0, 1).values,
+                            items=Yv,
+                            major_axis=t,
+                            minor_axis=Xv)
